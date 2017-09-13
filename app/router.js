ebd7e8b11274d04d571eefc032b8e0e146a980e0
874099925@qq.com <you@example.com>
2017-09-13 13:00:23 +0800

    fix

--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('about');  //新增route
 });
 
 export default Router;
---
+++ tests/acceptance/list-rentals-test.js
@@ -0,0 +1,6 @@
+// import { test } from 'qunit';
+import moduleForAcceptance from 'emberpro/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | list rentals');
+
+
